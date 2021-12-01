18bccd2fce4082ec8d296e2f66a70ec78ad1e41c
tommaso <37435103+Tommimon@users.noreply.github.com>
2021-12-01 06:10:09 +0100

    Day 1 solution

--- tommimon/d01/myClass.java
+++ tommimon/d01/myClass.java
@@ -1,7 +1,35 @@
 package tommimon.d01;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Vector;
+
 public class myClass {
-    public static void main(String[] args) {
-        System.out.println("giorno 1");
+    public static void main(String[] args) throws FileNotFoundException {
+        Vector<Integer> nums = new Vector<>();
+        File myObj = new File("tommimon/d01/input");
+        Scanner myReader = new Scanner(myObj);
+        while(myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            nums.add(Integer.parseInt(data));
+        }
+        myReader.close();
+
+        int counter = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            if(nums.get(i) > nums.get(i-1)) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
+
+        counter = 0;
+        for (int i = 3; i < nums.size(); i++) {
+            if(nums.get(i) > nums.get(i-3)) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
     }
 }
