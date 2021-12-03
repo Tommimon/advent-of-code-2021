c13823d1798fe92cd2570f4462e835a75d9115d3
tommaso <37435103+Tommimon@users.noreply.github.com>
2021-12-03 15:13:17 +0100

    Set executable bit

--- hooks.py
+++ hooks.py
@@ -1,4 +1,7 @@
 import pathlib
+import platform
+import os
+import stat
 
 text = '''# update leaderboard before commit
 python3 leaderboard.py
@@ -9,5 +12,10 @@ path = pathlib.Path('.git/hooks/pre-commit')
 fp = open(path, 'w')
 fp.write(text)
 fp.close()
+
+if platform.system() != 'Windows':
+    st = os.stat(path)
+    os.chmod(path, st.st_mode | stat.S_IEXEC)
+
 
 print("Hooks setup finished!")
