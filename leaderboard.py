a04410308416caf5f89c99bb0cb376b2998ad848
tommaso <37435103+Tommimon@users.noreply.github.com>
2021-12-04 21:25:54 +0100

    Moved cookies request to setup.py

--- leaderboard.py
+++ leaderboard.py
@@ -1,4 +1,3 @@
-from os.path import exists
 import time
 try:
     import requests
@@ -10,12 +9,6 @@ URL = 'https://adventofcode.com/2021/leaderboard/private/view/976110.json'
 COOKIE_FILE = 'cookies'
 LEADERBOARD_FILE = 'leaderboard.json'
 DELTA_TIME = 900
-
-
-def update_cookies():
-    cookies = input("Enter new cookies: ")
-    with open(COOKIE_FILE, 'w') as output_file:
-        output_file.write(cookies)
 
 
 def update_leaderboard():
@@ -30,10 +23,10 @@ def update_leaderboard():
             with open(COOKIE_FILE, "r") as input_file:
                 cookies = input_file.readlines()[0]
             headers = {"Cookie": cookies}
-        except IndexError:
+            print("Cookies loaded successfully")
+        except (IndexError, FileNotFoundError):
             print("Cookies not found")
             return False
-        print("Cookies loaded successfully")
 
         txt = requests.get(URL, headers=headers).text
         if txt.find('"owner_id":"976110"') == -1:  # check leaderboard
@@ -48,16 +41,7 @@ def update_leaderboard():
     return True
 
 
-if not exists(COOKIE_FILE):
-    print("Cookies not found")
-    update_cookies()
-
-# if something goes wring update cookies and retry
 if not update_leaderboard():
-    update_cookies()
-    if update_leaderboard():
-        print("Leaderboard is up to date")
-    else:
-        print("Couldn't update the leaderboard")
+    print("Couldn't update the leaderboard")
 else:
     print("Leaderboard is up to date")
---
+++ setup.py
@@ -0,0 +1,42 @@
+import pathlib
+import platform
+import os
+import stat
+
+text = '''#!/bin/sh
+# update leaderboard before commit
+python3 leaderboard.py
+git add leaderboard.json
+'''
+COOKIE_FILE = 'cookies'
+
+if platform.system() == 'Windows':
+    text = text.replace('python3', 'python')
+
+path = pathlib.Path('.git/hooks/pre-commit')
+fp = open(path, 'w')
+fp.write(text)
+fp.close()
+
+if platform.system() != 'Windows':
+    st = os.stat(path)
+    os.chmod(path, st.st_mode | stat.S_IEXEC)
+
+
+def write_cookies(new_cookies):
+    with open(COOKIE_FILE, 'w') as output_file:
+        output_file.write(new_cookies.strip("Cookie: "))
+
+
+try:
+    with open(COOKIE_FILE, "r") as input_file:
+        current = input_file.readlines()[0]
+        cookies = input("New cookies (press ENTER to keep current): ")
+        if cookies != "":
+            write_cookies(cookies)
+except (IndexError, FileNotFoundError):
+    cookies = input("Cookies not found, enter your session cookies: ")
+    write_cookies(cookies)
+
+
+print("Hooks setup finished!")
