a430a00c4a256cdfbb8fec44de0d8192f4257e7e
marcomole00 <57618578+marcomole00@users.noreply.github.com>
2021-12-06 18:28:18 +0100

    day 6

---
+++ marcomole00/d06/Part2.java
@@ -0,0 +1,46 @@
+package marcomole00.d06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Part2 {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = Files.newBufferedReader(Paths.get("marcomole00/d06/input"));
+            String stringa = br.lines().toList().get(0);
+            ArrayList<Integer> numeri = new ArrayList<Integer>();
+            Arrays.stream(stringa.split(",")).map(Integer::parseInt).forEach(numeri::add);
+
+            long[] numeriArray = new long[9];
+            numeri.stream().forEach(i -> numeriArray[i]++);
+            //for (long i : numeriArray){System.out.print(i);}
+            long numberOfZeros =0;
+
+            for (int j = 0; j<256; j++)
+            {
+
+                //for (long i : numeriArray){System.out.print(i);}
+              // System.out.println();
+
+                for(int k =0;k<8;k++)
+                {
+                    numeriArray[k] = numeriArray[k+1];
+                }
+                numeriArray[8] = numberOfZeros;
+                numeriArray[6] += numberOfZeros;
+                numberOfZeros = numeriArray[0];
+
+
+            }
+
+            System.out.println(Arrays.stream(numeriArray).reduce(0, Long::sum));
+
+        } catch (IOException ignored){}
+
+
+    }
+}
---
+++ marcomole00/d06/day6.java
@@ -0,0 +1,59 @@
+package marcomole00.d06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class day6 {
+
+    public static int regenerate(int num)
+    {
+        if (num == 0) return 7;
+        return num;
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = Files.newBufferedReader(Paths.get("marcomole00/d06/test"));
+            String stringa = br.lines().toList().get(0);
+            ArrayList<Integer>  numeri = new ArrayList<Integer>();
+            Arrays.stream(stringa.split(",")).map(Integer::parseInt).forEach(numeri::add);
+            long temp = 0;
+            for (int i = 0; i<80;i++)
+            {
+                for (int k : numeri)
+                {
+                  //  System.out.print(k + " ");
+                }
+
+
+                //System.out.println();
+                numeri =  numeri.stream().map(day6::regenerate).map(n -> n-1).collect(Collectors.toCollection(ArrayList::new));
+                for (long j = 0; j < temp; j++)
+                {
+                    numeri.add(8);
+                }
+                temp =  numeri.stream().filter(n -> n == 0).count();
+
+            }
+
+
+
+
+        System.out.println(numeri.stream().count());
+
+
+
+
+        } catch (IOException E)
+        {
+            System.out.println("bello di mamma");
+        }
+    }
+}
