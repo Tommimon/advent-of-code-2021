f2560288e24ab417258cb35f967277aaf3a67f78
Gonduls <pierluigi.negro.00@gmail.com>
2021-12-08 11:31:19 +0100

    Revert "failed attempt, might try to continue eventually"
    This reverts commit 8a4ab0e47c3fd2b27f8f00b05d071390a379be09.

--- Gonduls/d08/ConversionIncompleteException.java
+++
@@ -1,7 +0,0 @@
-package Gonduls.d08;
-
-public class ConversionIncompleteException extends Exception{
-    public ConversionIncompleteException(){
-        super("You still need to convert some letters");
-    }
-}
