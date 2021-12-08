8a4ab0e47c3fd2b27f8f00b05d071390a379be09
Gonduls <pierluigi.negro.00@gmail.com>
2021-12-08 11:29:49 +0100

    failed attempt, might try to continue eventually

---
+++ Gonduls/d08/ConversionIncompleteException.java
@@ -0,0 +1,7 @@
+package Gonduls.d08;
+
+public class ConversionIncompleteException extends Exception{
+    public ConversionIncompleteException(){
+        super("You still need to convert some letters");
+    }
+}
