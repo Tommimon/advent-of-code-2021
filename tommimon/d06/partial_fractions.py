1fc289307c924a7ae825f7047378575a0ae81564
tommaso <37435103+Tommimon@users.noreply.github.com>
2021-12-09 17:57:16 +0100

    More comments

--- tommimon/d06/partial_fractions.py
+++ tommimon/d06/partial_fractions.py
@@ -1,5 +1,15 @@
 import numpy as np
 
+'''
+Decomposing F(z), the generating function of f(n) where f(n) is the number of fish with lifetime 0 at step n starting
+with only one fish with lifetime 6
+f(0)=0, f(1)=0, f(2)=0, f(3)=0, f(4)=0, f(5)=0, f(6)=1, f(7)=0, f(8)=0, f(n)=f(n-7)+f(n-9)
+F(z) = sum[f(n)*z^n] for n: 0->inf
+F(z) = f(6)*z^6 + sum[(f(n-7)+f(n-9))*z^n] for n: 9->inf
+F(z) = z^6 + z^7*F(z) + z^9*F(z)
+F(z) = z^6/(1-z^9-z^7)
+'''
+
 # Enter the coefficients of the poly in the array
 NUM = [0, 0, 1, 0, 0, 0, 0, 0, 0]
 DEN = [-1, 0, -1, 0, 0, 0, 0, 0, 0, 1]
