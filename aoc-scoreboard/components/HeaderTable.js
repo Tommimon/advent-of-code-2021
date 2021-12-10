f1cd2d48263a249a14be0c1e957fbc606f8ed600
riccardo-negri <67798955+riccardo-negri@users.noreply.github.com>
2021-12-10 16:29:20 +0100

    Added local score

--- aoc-scoreboard/components/HeaderTable.js
+++ aoc-scoreboard/components/HeaderTable.js
@@ -19,7 +19,9 @@ export default function HeaderTable(props) {
             )}
           </p>
           <p class="flex items-center justify-center">
-            ({value.stars}{" "}
+            ({value.local_score}
+            {"-"}
+            {value.stars}{" "}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               class="h-4 w-4 text-yellow-400"
