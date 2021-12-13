34fb2f0f5b216fdb3f8b5bf5596d26743c1e4143
marcomole00 <57618578+marcomole00@users.noreply.github.com>
2021-12-13 12:54:56 +0100

    day13-polished

--- marcomole00/d13/day13.java
+++ marcomole00/d13/day13.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
-import java.util.OptionalInt;
 
 public class day13 {
     public static void main(String[] args) {
@@ -17,25 +15,13 @@ public class day13 {
             int[] coordy = br.lines().filter(s->s.matches("\\d*,\\d*")).map(s -> s.split(",")[1]).mapToInt(Integer::parseInt).toArray();
             br = Files.newBufferedReader(Paths.get("marcomole00/d13/input")); // this is idiotic
             List<String> creases= br.lines().filter(s->s.matches("fold along [yx]=\\d*")).map(s -> s.split(" ")[2]).toList();
-            creases.forEach(System.out::println);
 
-
-
-
+            
             int sizex = 2* creases.stream().filter(s -> s.matches("x=\\d*")).mapToInt(s -> Integer.parseInt(s.split("=")[1])).reduce(Integer.MIN_VALUE,Integer::max)+1;
             int sizey = 2 * creases.stream().filter(s -> s.matches("y=\\d*")).mapToInt(s -> Integer.parseInt(s.split("=")[1])).reduce(Integer.MIN_VALUE,Integer::max) +1;
 
-
-            for(int x : coordx){System.out.print(x +" ");}
-            System.out.println("max = " + sizex);
-            for(int y : coordy){System.out.print(y +" ");}
-            System.out.println("max = " + sizey);
+            //apparently is initialized at false, cool
             boolean[][] matrix = new boolean[sizey][sizex];
-            for (int i = 0; i<sizey;i++)
-            {
-                for (int j =0; j <sizex;j++){matrix[i][j] = false;}
-            }
-
 
             assert coordx.length == coordy.length;
 
@@ -44,26 +30,11 @@ public class day13 {
                 matrix[coordy[i]][coordx[i]] = true;
             }
 
-            for (int i = 0; i<sizey;i++) {
-                for (int j = 0; j < sizex; j++) {
-                    if (matrix[i][j]) {
-                       ///System.out.print("█");
-                    } else {
-                      //  System.out.print("-");
-                    }
-                }
-               // System.out.println();
-
-            }
-
-            //first crease
             int numberOfPoint =0;
             boolean first = false;
             for (String crease : creases) {
-
                 if (crease.charAt(0) == 'y') {
-                    //int whereToCrease = Integer.parseInt(creases.get(0).split("=")[1]);
-                    System.out.println(crease.charAt(0));
+
                     for (int i = 0; i < sizey / 2; i++) {
                         for (int j = 0; j < sizex; j++) {
                             matrix[i][j] = matrix[i][j] || matrix[sizey - 1 - i][j];
@@ -72,8 +43,6 @@ public class day13 {
                     }
                     sizey = sizey / 2;
                 } else {
-                   // int whereToCrease = Integer.parseInt(creases.get(0).split("=")[1]);
-                    System.out.println(crease.charAt(0));
                     for (int i = 0; i < sizey; i++) {
                         for (int j = 0; j < sizex / 2; j++) {
                             matrix[i][j] = matrix[i][j] || matrix[i][sizex - 1 - j];
@@ -83,48 +52,20 @@ public class day13 {
                     }
                     sizex = sizex / 2;
                 }
-
                 if (!first) {System.out.println(numberOfPoint); first = true;}
-
-
-
             }
-
-
-
-
-
+            
            for (int i = 0; i<sizey;i++) {
                for (int j = 0; j < sizex; j++) {
                    if (matrix[i][j]) {
                        System.out.print("█");
-                       ++numberOfPoint;
                    } else {
-                      System.out.print("-");
+                      System.out.print(" ");
                    }
-
                }
                System.out.println();
            }
-
-
-               System.out.println(numberOfPoint);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        } catch (IOException iognored){}
+           
+        } catch (IOException ignored){}
     }
 }
