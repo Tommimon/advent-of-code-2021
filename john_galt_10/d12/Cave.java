5c2bb5d334cdc02dad13f3c19b95cf001de7baa6
john-galt-10 <95347137+john-galt-10@users.noreply.github.com>
2021-12-16 22:34:10 +0100

    day 12 in ritardo di 4 giorni - questo potrebbe essere il mio canto del cigno

--- john_galt_10/d12/Cave.java
+++ john_galt_10/d12/Cave.java
@@ -1,18 +1,19 @@
 package john_galt_10.d12;
 
+import javax.swing.text.html.HTMLDocument;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cave {
     private String name;
     private boolean isSmall;
-    private boolean isVisited;
+    private boolean visited;
     private List<Cave> adiacenti;
 
     public Cave (String name) {
         this.name = name;
-        if ('A' <= name.charAt(0) && 'Z' >= name.charAt(0)) isSmall = true;
-        isVisited = false;
+        if (!('A' <= name.charAt(0) && 'Z' >= name.charAt(0))) isSmall = true;
+        visited = false;
         adiacenti = new ArrayList<>();
     }
 
@@ -24,6 +25,16 @@ public class Cave {
         return name;
     }
 
+    public void setVisited(boolean visited) { this.visited = visited; }
+
+    public boolean isVisited() { return visited; }
+
+    public boolean isSmall() { return isSmall; }
+
+    public List<Cave> getAdiacenti() {
+        return adiacenti;
+    }
+
     public boolean equals(Object o) {
         if (!(o instanceof Cave)) return false;
         Cave c = (Cave) o;
@@ -33,11 +44,11 @@ public class Cave {
     @Override
     public String toString() {
         String sAdiacenti = "";
-        for (Cave c : adiacenti) sAdiacenti += c.getName();
+        for (Cave c : adiacenti) sAdiacenti += (" " + c.getName());
         return "Cave{" +
                 "name='" + name + '\'' +
                 ", isSmall=" + isSmall +
-                ", isVisited=" + isVisited +
+                ", isVisited=" + visited +
                 ", adiacenti=" + sAdiacenti +
                 '}';
     }
--- john_galt_10/d12/Main.java
+++ john_galt_10/d12/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        File myObj = new File("john_galt_10/d12/test.txt");
+        File myObj = new File("john_galt_10/d12/input.txt");
         Scanner myReader = new Scanner(myObj);
         int sol = parteUno(myReader);
         //int sol = parteDue(myReader);
@@ -21,7 +21,8 @@ public class Main {
         int sol = 0;
         List<Cave> caves = new ArrayList<>();
 
-
+        //creates caves map
+        //metti in cima all'input un percorso che inizia con start che sono pigro
         while (reader.hasNextLine()) {
             String input = reader.nextLine();
             String from = input.split("-")[0];
@@ -30,30 +31,86 @@ public class Main {
 
             Cave n = new Cave(from);
 
-            System.out.println(caves.contains(n));
             if (caves.contains(n)) {
-                System.out.println("c'è");
                 caves.get(caves.indexOf(n)).addAdiacente(new Cave(to));
             } else {
                 n.addAdiacente(new Cave(to));
                 caves.add(n);
-                System.out.println("aggiunto " + from);
             }
-            //(from, to) = input.split("-");
+
+            n = new Cave(to);
+            if (caves.contains(n)) {
+                caves.get(caves.indexOf(n)).addAdiacente(new Cave(from));
+            } else {
+                n.addAdiacente(new Cave(from));
+                caves.add(n);
+            }
         }
-
-
-
-        String s = "start";
-        Cave n = new Cave(s);
-        caves.add(n);
-        Cave nn = new Cave(s);
-        System.out.println(caves.contains(nn));
-
 
         for (Cave c : caves) System.out.println(c);
 
+        //sol = countPaths(caves.get(0), caves, new ArrayList<String>());
+        sol = countPathsDue(caves.get(0), caves, new ArrayList<String>());
+
         return sol;
+    }
+
+    private static int countPaths(Cave start, List<Cave> caves, List<String> path) {
+        int sol = 0;
+
+        if (start.isSmall() && path.contains(start.getName())) {
+            return 0;
+        }
+        if (start.getName().equals("end")) {
+            return 1;
+        }
+
+        List<String> newPath = new ArrayList<>();
+        for (String s : path) {
+            newPath.add(s);
+        }
+        newPath.add(start.getName());
+        System.out.println("");
+
+        for (Cave c : start.getAdiacenti()) {
+            sol += countPaths(caves.get(caves.indexOf(c)), caves, newPath);
+        }
+        return sol;
+    }
+
+    private static int countPathsDue(Cave start, List<Cave> caves, List<String> path) {
+        int sol = 0;
+
+        if (start.isSmall() && path.contains(start.getName())) {
+            if (start.getName().equals("start") || start.getName().equals("end") || alreadyTwo(path)) return 0;
+        }
+        if (start.getName().equals("end")) {
+            return 1;
+        }
+
+        List<String> newPath = new ArrayList<>();
+        for (String s : path) {
+            newPath.add(s);
+        }
+        newPath.add(start.getName());
+
+        for (Cave c : start.getAdiacenti()) {
+            sol += countPathsDue(caves.get(caves.indexOf(c)), caves, newPath);
+        }
+        return sol;
+    }
+
+    private static boolean alreadyTwo(List<String> path) {
+
+
+        for (int i = 0; i < path.size(); i++) {
+            for (int j = 0; j < path.size(); j++) {
+                if (path.get(i).charAt(0) >= 'a' && path.get(i).charAt(0) <= 'z' && j!=i && path.get(i).equals(path.get(j))) return true;
+            }
+        }
+
+
+        return false;
     }
 }
 
