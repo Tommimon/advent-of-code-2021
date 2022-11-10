40de93a438d51fd22cc96128011e42cea4edcc44
Gonduls <pierluigi.negro.00@gmail.com>
2022-11-10 23:24:13 +0100

    deals with part 1 in under 5 minutes!

--- Gonduls/d24/part1.py
+++ Gonduls/d24/part1.py
@@ -31,52 +31,50 @@ def evaluate(operation, varA: set, varB: set=None) -> set:
     
     return result
 
-lines = getInput()
-for _ in range(14):
-    found = False
-    for i in range(9, 0, -1):
-        #print('i = ', i)
-        first = -1
-        x, y, w, z = {0}, {0}, {0}, {0}
-        for num, line in enumerate(lines):
-            line = line.strip()
-            b = None
-            if len(line.split(' ')) > 2:
-                match line.split(' ')[2]:
-                    case 'x': b = x
-                    case 'y': b = y
-                    case 'w': b = w
-                    case 'z': b = z
-                    case _:
-                        b = set()
-                        b.add(int(line.split(' ')[2]))
-            
-            oper = line.split(' ')[0]
-            match line.split(' ')[1]:
-                case 'x':
-                    a = x
-                    x = evaluate(oper, x, b)
-                case 'y':
-                    a = y
-                    y = evaluate(oper, y, b)
-                case 'w':
-                    a = w
-                    w = evaluate(oper, w, b)
-                case 'z':
-                    a = z
-                    z = evaluate(oper, z, b)
-            if oper == 'imp':
-                if first< 0:
-                    first = num
-                    a.empty()
-                    a.add(i)
+def solve(lines: list, x: set={0}, y: set={0}, z: set={0}, w: set={0}, limit:int = 6) -> str:
+
+    for num, line in enumerate(lines):
+        #print(line)
+        line = line.strip()
+        b = None
+        if len(line.split(' ')) > 2:
+            match line.split(' ')[2]:
+                case 'x': b = x
+                case 'y': b = y
+                case 'w': b = w
+                case 'z': b = z
+                case _:
+                    b = set()
+                    b.add(int(line.split(' ')[2]))
         
-        if 0 in z:
-            print(i, end ='')
-            lines[num] = 'mul w 0'
-            lines.insert(num + 1, 'add w ' + str(i))
-            found = True
-            break
-    if not found:
-        print('Not found, exit')
-        break
+        oper = line.split(' ')[0]
+        match line.split(' ')[1]:
+            case 'x': x = evaluate(oper, x, b)
+            case 'y': y = evaluate(oper, y, b)
+            case 'w': w = evaluate(oper, w, b)
+            case 'z': z = evaluate(oper, z, b)
+        if oper == 'inp':
+            if limit == 0:
+                w = set(range(1, 10))
+                continue
+
+            #for i in range(1, 10):
+            for i in range(9, 0, -1):
+                #print(' ' * limit, i)
+                w = {i}
+                res = solve(lines[num + 1:], x, y, z, w, limit -1)
+                if len(res) == 0:
+                    continue
+
+                if res == ' ':
+                    #return str(i)
+                    res = solve(lines[num + 1:], x, y, z, w, 8)
+                return str(i) + res
+        
+    if 0 in z:
+        return ' '
+    return ''
+
+if __name__ == '__main__':
+    lines = getInput()
+    print('Result part 1: ', solve(lines))
